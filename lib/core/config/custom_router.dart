725351c3766b867667ae57df400d7148c11c0bf1
dadzi <dadziemikke2@gmail.com>
2022-01-27 10:02:40 +0000

    Added custom router

---
+++ lib/core/config/custom_router.dart
@@ -0,0 +1,38 @@
+import 'package:bluebaker/core/onboarding/pages/onboard.dart';
+import 'package:bluebaker/core/screens/error.dart';
+import 'package:bluebaker/core/screens/splash.dart';
+import 'package:flutter/material.dart';
+
+class CustomRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    print('Route: ${settings.name}');
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: '/'),
+          builder: (_) => const Scaffold(),
+        );
+      case OnboardScreen.routeName:
+        return OnboardScreen.route();
+      case SplashScreen.routeName:
+        return SplashScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route onGenerateNestedRoute(RouteSettings settings) {
+    print('NestedRoute: ${settings.name}');
+    switch (settings.name) {
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: '/error'),
+      builder: (_) => const ErrorScreen(),
+    );
+  }
+}
