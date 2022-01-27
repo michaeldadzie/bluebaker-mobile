091df992bb90068830859ebc42a0744871517dec
dadzi <dadziemikke2@gmail.com>
2022-01-27 10:07:51 +0000

    Added BlueBaker to main function

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/utils/screen_sizes.dart';
+import 'core/config/custom_router.dart';
+import 'core/onboarding/pages/onboard.dart';
+import 'core/screens/splash.dart';
+import 'core/utils/theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  EquatableConfig.stringify = kDebugMode;
+  await Firebase.initializeApp();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // status bar color
+    ),
+  );
+  runApp(const BlueBaker());
+}
+
+class BlueBaker extends StatelessWidget {
+  const BlueBaker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(
+        MyScreenSizes.screenWidth,
+        MyScreenSizes.screenHeight,
+      ),
+      builder: () {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: Constants.lightTheme,
+          darkTheme: Constants.darkTheme,
+          onGenerateRoute: CustomRouter.onGenerateRoute,
+          initialRoute: SplashScreen.routeName,
+        );
+      },
+    );
+  }
+}
