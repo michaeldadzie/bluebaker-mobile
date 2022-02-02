50ac1db7ab4f960306aa3326669cc2f9e8eba6a8
dadzi <dadziemikke2@gmail.com>
2022-02-02 13:57:33 +0000

    Added login cubit

---
+++ lib/features/auth/presentation/cubit/login/login_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import '/features/auth/data/models/failure.dart';
+import '/features/auth/data/repositories/auth_repository.dart';
+
+
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final AuthRepository? _authRepository;
+  // ignore: invalid_required_positional_param
+  LoginCubit({
+    required AuthRepository? authRepository,
+  })  : _authRepository = authRepository,
+        super(LoginState?.initial());
+
+  void emailChanged(String? value) {
+    emit(state.copyWith(email: value, status: LoginStatus.initial));
+  }
+
+  void passwordChanged(String? value) {
+    emit(state.copyWith(password: value, status: LoginStatus.initial));
+  }
+
+  void logInWithCredentials() async {
+    if (!state.isFormValid || state.status == LoginStatus.submitting) return;
+    emit(state.copyWith(status: LoginStatus.submitting));
+    try {
+      await _authRepository?.logInWithEmailAndPassword(
+        email: state.email,
+        password: state.password,
+      );
+      emit(state.copyWith(status: LoginStatus.success));
+    } on Failure catch (error) {
+      emit(state.copyWith(failure: error, status: LoginStatus.error));
+    }
+  }
+}
