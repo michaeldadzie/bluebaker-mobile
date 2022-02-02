ec865b2e146fbdd04b15a006774050a6c2ae7179
dadzi <dadziemikke2@gmail.com>
2022-02-02 14:09:22 +0000

    Added auth screen routes to custom router

--- lib/core/config/custom_router.dart
+++ lib/core/config/custom_router.dart
@@ -2,6 +2,9 @@ import 'package:bluebaker/core/nav/page/bottom_nav_screen.dart';
 import 'package:bluebaker/core/onboarding/pages/onboard.dart';
 import 'package:bluebaker/core/screens/error.dart';
 import 'package:bluebaker/core/screens/splash.dart';
+import 'package:bluebaker/features/auth/presentation/pages/login.dart';
+import 'package:bluebaker/features/auth/presentation/pages/signup.dart';
+import 'package:bluebaker/features/auth/presentation/pages/welcome.dart';
 import 'package:flutter/material.dart';
 
 class CustomRouter {
@@ -15,8 +18,14 @@ class CustomRouter {
         );
       case OnboardScreen.routeName:
         return OnboardScreen.route();
+      case WelcomeScreen.routeName:
+        return WelcomeScreen.route();
       case SplashScreen.routeName:
         return SplashScreen.route();
+      case SignupScreen.routeName:
+        return SignupScreen.route();
+      case LoginScreen.routeName:
+        return LoginScreen.route();
       case BottomNavScreen.routeName:
         return BottomNavScreen.route();
       default:
