c3b381768d78710f292c1765a02d5e68575bdf3e
dadzi <dadziemikke2@gmail.com>
2022-02-02 14:02:55 +0000

    Extracted textFormFieldDecoration

---
+++ lib/features/auth/presentation/utils/const.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final textFormFieldDecoration = InputDecoration(
+  hintText: '',
+  filled: true,
+  hintStyle: GoogleFonts.lato(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey.shade600,
+  ),
+  errorStyle: GoogleFonts.lato(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: Colors.red,
+  ),
+  errorBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: const BorderSide(color: Colors.transparent),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: const BorderSide(color: Colors.transparent),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: const BorderSide(color: Colors.transparent),
+  ),
+  disabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: const BorderSide(color: Colors.transparent),
+  ),
+);
