019a21dafb41689da9d489be0d8b9ef3f82d506e
dadzi <dadziemikke2@gmail.com>
2022-02-02 14:11:14 +0000

    Added BlocListener to check if user is authenticated or not

--- lib/core/screens/splash.dart
+++ lib/core/screens/splash.dart
@@ -1,5 +1,8 @@
-import 'package:bluebaker/core/nav/page/bottom_nav_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '/core/nav/page/bottom_nav_screen.dart';
+import '/features/auth/presentation/bloc/auth/auth_bloc.dart';
+import '/features/auth/presentation/pages/welcome.dart';
 
 class SplashScreen extends StatelessWidget {
   static const String routeName = '/splash';
@@ -16,14 +19,21 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              // Future.delayed(const Duration(seconds: 1), () {
+      child: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state.status == AuthStatus.unauthenticated) {
+            //Go to Welcome Screen
+            Navigator.of(context).pushNamed(WelcomeScreen.routeName);
+          } else if (state.status == AuthStatus.authenticated) {
+            // Go to Bottom Nav Bar
+            Future.delayed(const Duration(seconds: 0), () {
               Navigator.of(context).pushNamed(BottomNavScreen.routeName);
-              // });
-            },
+            });
+          }
+          print(state);
+        },
+        child: Scaffold(
+          body: Center(
             child: CircularProgressIndicator(
               color: Colors.blue.shade600,
             ),
