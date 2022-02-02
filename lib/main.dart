2caf83a0e81f57c1a09bd3da24a272e5e7ae9f19
dadzi <dadziemikke2@gmail.com>
2022-02-02 14:36:33 +0000

    Wrapped MaterialApp with MultiBlocProvider and MultiRepository Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:bluebaker/core/utils/screen_sizes.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'core/onboarding/pages/onboard.dart';
 import 'core/screens/splash.dart';
-import 'core/utils/screen_sizes.dart';
 import 'core/config/custom_router.dart';
 import 'core/utils/theme.dart';
+import 'features/auth/data/repositories/auth_repository.dart';
+import 'features/auth/presentation/bloc/auth/auth_bloc.dart';
+import 'features/auth/presentation/bloc/observer/simple_bloc_observer.dart';
 
 late int? onboard;
 Future<void> main() async {
@@ -19,35 +23,61 @@ Future<void> main() async {
   await prefs.setInt("onboard", 1);
   EquatableConfig.stringify = kDebugMode;
   await Firebase.initializeApp();
+  Bloc.observer = SimpleClassObserver();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent, // status bar color
     ),
   );
-  runApp(const BlueBaker());
+  runApp(
+    const BlueBaker(),
+  );
 }
 
 class BlueBaker extends StatelessWidget {
   const BlueBaker({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: Size(
-        MyScreenSizes.screenWidth,
-        MyScreenSizes.screenHeight,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthRepository>(
+          create: (_) => AuthRepository(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+            create: (context) => AuthBloc(
+              authRepository: context.read<AuthRepository>(),
+            ),
+          )
+        ],
+        child: ScreenUtilInit(
+          designSize: Size(
+            MyScreenSizes.screenWidth,
+            MyScreenSizes.screenHeight,
+          ),
+          builder: () {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              builder: (context, widget) {
+                ScreenUtil.setContext(context);
+                return MediaQuery(
+                  data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                  child: widget!,
+                );
+              },
+              theme: Constants.lightTheme,
+              darkTheme: Constants.darkTheme,
+              onGenerateRoute: CustomRouter.onGenerateRoute,
+              initialRoute: onboard == 0 || onboard == null
+                  ? OnboardScreen.routeName
+                  : SplashScreen.routeName,
+            );
+          },
+        ),
       ),
-      builder: () {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: Constants.lightTheme,
-          darkTheme: Constants.darkTheme,
-          onGenerateRoute: CustomRouter.onGenerateRoute,
-          initialRoute: OnboardScreen.routeName,
-          // initialRoute: onboard == 0 || onboard == null
-          //     ? OnboardScreen.routeName
-          //     : SplashScreen.routeName,
-        );
-      },
     );
   }
 }
