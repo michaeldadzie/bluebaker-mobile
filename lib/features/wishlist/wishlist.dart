70eb8e79bfeed91137675ce65c82f021e986a495
dadzi <dadziemikke2@gmail.com>
2022-02-03 14:20:48 +0000

    Refactored code

--- lib/features/wishlist/wishlist.dart
+++ lib/features/wishlist/wishlist.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '/features/auth/presentation/bloc/auth/auth_bloc.dart';
 
 class WishList extends StatelessWidget {
   const WishList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
