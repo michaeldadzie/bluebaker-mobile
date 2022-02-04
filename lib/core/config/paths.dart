d3576fca264fb750f5ea4ec1d9f9e68f0af07074
dadzi <dadziemikke2@gmail.com>
2022-02-04 14:40:17 +0000

    Added new top level and subcollection Firestore Collection paths

---
+++ lib/core/config/paths.dart
@@ -0,0 +1,10 @@
+class Paths {
+  // top level collection
+  static const String users = 'users';
+  static const String items = 'items';
+  static const String collections = 'collections';
+  static const String favoriteItems = 'favorite-items';
+
+  // subcollections
+  static const String userFavoriteItems = 'userFavoriteItems';
+}
