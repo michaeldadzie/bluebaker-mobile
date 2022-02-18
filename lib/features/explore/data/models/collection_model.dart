3c813c59d93a5a984d5d451595fee5493e5285d9
dadzi <dadziemikke2@gmail.com>
2022-02-18 12:21:38 +0000

    Added collection model

---
+++ lib/features/explore/data/models/collection_model.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+class Collection extends Equatable {
+  const Collection({
+    required this.id,
+    required this.photoUrl,
+    required this.title,
+  });
+
+  final String id;
+  final String photoUrl;
+  final String title;
+
+  @override
+  List<Object?> get props => [id, photoUrl, title];
+
+  Collection copyWith({
+    String? id,
+    String? photoUrl,
+    String? title,
+  }) {
+    return Collection(
+      id: id ?? this.id,
+      photoUrl: photoUrl ?? this.photoUrl,
+      title: title ?? this.title,
+    );
+  }
+
+  Map<String, dynamic> toDocument() {
+    return {
+      'title': title,
+      'photoUrl': photoUrl,
+      'id': id,
+    };
+  }
+
+  factory Collection.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Collection(
+      id: data['id'] ?? '',
+      photoUrl: data['photoUrl'] ?? '',
+      title: data['title'] ?? '',
+    );
+  }
+}
