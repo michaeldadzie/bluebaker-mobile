4407f89777410e51f105d83611b60cb1bad74f16
dadzi <dadziemikke2@gmail.com>
2022-02-19 12:29:02 +0000

    Added bluebaker firestore path

--- lib/core/config/paths.dart
+++ lib/core/config/paths.dart
@@ -2,6 +2,7 @@ class Paths {
   // top level collection
   static const String users = 'users';
   static const String items = 'items';
+  static const String bluebaker = 'bluebaker';
   static const String collections = 'collections';
   static const String favoriteItems = 'favorite-items';
 
