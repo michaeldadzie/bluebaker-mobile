095699979b768a965e2d13cab4274ac92d572acd
dadzi <dadziemikke2@gmail.com>
2022-03-06 10:36:41 +0000

    Added BlueBakerLife Screen

---
+++ lib/features/bluebaker/presentation/pages/bluebaker/bluebaker_life.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../collections.dart';
+
+
+class BlueBakerLife extends StatelessWidget {
+  static const String routeName = '/bluebaker-life';
+  const BlueBakerLife({Key? key}) : super(key: key);
+
+  static Route route() {
+    return CupertinoPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const BlueBakerLife(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/bluelife.gif'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black26,
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 80.h,
+            left: 50.w,
+            right: 50.w,
+            bottom: 80.h,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'About',
+                        style: GoogleFonts.lato(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushNamed(
+                            Collections.routeName,
+                          );
+                        },
+                        child: Text(
+                          'Collections',
+                          style: GoogleFonts.lato(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Stories',
+                        style: GoogleFonts.lato(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Products',
+                        style: GoogleFonts.lato(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
