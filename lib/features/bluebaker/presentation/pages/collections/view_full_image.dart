cc3989aabb7c3f1a0d36beb7d26a36e1b55e7ac4
dadzi <dadziemikke2@gmail.com>
2022-03-06 10:28:30 +0000

    Added ViewFullImage Screen

---
+++ lib/features/bluebaker/presentation/pages/collections/view_full_image.dart
@@ -0,0 +1,58 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ViewFullImage extends StatelessWidget {
+  static const String routeName = '/image';
+  const ViewFullImage({
+    Key? key,
+    required this.photoUrl,
+  }) : super(key: key);
+
+  final String photoUrl;
+
+  static Route route(final String photoUrl) {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => ViewFullImage(
+        photoUrl: photoUrl,
+      ),
+    );
+  }
+
+  // @override
+  // void initState() {
+  //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  //   super.initState();
+  // }
+
+  // @override
+  // void dispose() {
+  //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () => Navigator.of(context).pop(),
+        child: Stack(
+          children: [
+            Hero(
+              tag: photoUrl,
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/load/load.gif'),
+                image: CachedNetworkImageProvider(photoUrl, scale: 1.0),
+                fadeInDuration: const Duration(milliseconds: 300),
+                fit: BoxFit.cover,
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
