9005bfa44256af1445393a662457fd9f557bd23d
dadzi <dadziemikke2@gmail.com>
2022-03-09 10:38:50 +0000

    Refactored Code

--- lib/core/config/custom_router.dart
+++ lib/core/config/custom_router.dart
@@ -1,20 +1,23 @@
-import 'package:bluebaker/core/nav/page/bottom_nav_screen.dart';
-import 'package:bluebaker/core/onboarding/pages/onboard.dart';
-import 'package:bluebaker/core/screens/error.dart';
-import 'package:bluebaker/core/screens/splash.dart';
-import 'package:bluebaker/features/account/presentation/pages/account_details.dart';
-import 'package:bluebaker/features/account/presentation/pages/edit_profile.dart';
-import 'package:bluebaker/features/auth/presentation/pages/login.dart';
-import 'package:bluebaker/features/auth/presentation/pages/signup.dart';
-import 'package:bluebaker/features/auth/presentation/pages/welcome.dart';
-import 'package:bluebaker/features/bluebaker/presentation/pages/bluebaker_category.dart';
-import 'package:bluebaker/features/explore/presentation/pages/collections/collection_category.dart';
-import 'package:bluebaker/features/explore/presentation/pages/collections/item_detail.dart';
-import 'package:bluebaker/features/explore/presentation/pages/search/search.dart';
-import 'package:bluebaker/features/explore/presentation/pages/videos/videos.dart';
-import 'package:bluebaker/features/explore/presentation/pages/collections/collections.dart';
-import 'package:bluebaker/features/wishlist/pages/user_wishlist.dart';
 import 'package:flutter/material.dart';
+import '../../core/nav/page/bottom_nav_screen.dart';
+import '../../core/onboarding/pages/onboard.dart';
+import '../../core/screens/error.dart';
+import '../../core/screens/splash.dart';
+import '../../features/account/presentation/pages/account_details.dart';
+import '../../features/account/presentation/pages/edit_profile.dart';
+import '../../features/account/presentation/pages/privacy_policy.dart';
+import '../../features/auth/presentation/pages/login.dart';
+import '../../features/auth/presentation/pages/signup.dart';
+import '../../features/auth/presentation/pages/welcome.dart';
+import '../../features/bluebaker/presentation/pages/bluebaker/bluebaker_category.dart';
+import '../../features/bluebaker/presentation/pages/bluebaker/bluebaker_life.dart';
+import '../../features/explore/presentation/pages/search.dart';
+import '../../features/explore/presentation/pages/search_trends.dart';
+import '../../features/bluebaker/presentation/pages/videos.dart';
+import '../../features/wishlist/pages/user_wishlist.dart';
+import '../../features/bluebaker/presentation/pages/collections/collection_category.dart';
+import '../../features/bluebaker/presentation/pages/collections.dart';
+import '../../features/bluebaker/presentation/pages/collections/item_detail.dart';
 
 class CustomRouter {
   static Route onGenerateRoute(RouteSettings settings) {
@@ -54,22 +57,23 @@ class CustomRouter {
         return AccountDetails.route(
           args: settings.arguments as AccountDetailsArgs,
         );
+      case PrivacyPolicy.routeName:
+        return PrivacyPolicy.route();
 
       // Bluebaker Routes
       case BlueBakerCategory.routeName:
         return BlueBakerCategory.route(
           args: settings.arguments as BlueBakerCategoryArgs,
         );
+      case BlueBakerLife.routeName:
+        return BlueBakerLife.route();
 
-      // Explore Routes
-      case Search.routeName:
-        return Search.route();
       case Collections.routeName:
         return Collections.route();
       case Videos.routeName:
         return Videos.route();
 
-      // Sub-Explore Routes
+      // Sub-BlueBaker Routes
       case CollectionCategory.routeName:
         return CollectionCategory.route(
           args: settings.arguments as CollectionCategoryArgs,
@@ -78,7 +82,15 @@ class CustomRouter {
         return ItemDetails.route(
           args: settings.arguments as ItemDetailsArgs,
         );
-        
+
+      // BlueBaker Routes
+      case Search.routeName:
+        return Search.route();
+      case SearchTrends.routeName:
+        return SearchTrends.route(
+          args: settings.arguments as SearchTrendsArgs,
+        );
+
       // Wishlist Routes
       case UserWishlist.routeName:
         return UserWishlist.route(
